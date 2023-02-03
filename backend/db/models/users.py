cc860c6172252df76f9d6025aac98309ea66bbf0
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-02-03 13:47:06 +0100

    added dependency for test db model

--- backend/db/models/users.py
+++ backend/db/models/users.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
 from sqlalchemy.orm import relationship
-
 from db.base_class import Base
+
+
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(), unique=True, nullable=False)
@@ -10,3 +11,5 @@ class User(Base):
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean, default=False)
     jobs = relationship("Job", back_populates="owner")
+
+
